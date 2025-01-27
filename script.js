e7e3f62d8f2b41c7a3540836f3f7a09957ee1c6b
Maylo0314 <maylo.elfrink@student.graafschapcollege.nl>
2025-01-27 19:50:59 +0100

    .html weg met js

--- script.js
+++ script.js
@@ -50,3 +50,8 @@ document.getElementById("pc-vragenlijst").addEventListener("submit", function (e
     const modal = new bootstrap.Modal(document.getElementById("aanbevelingModal"));
     modal.show();
 });
+
+if (window.location.pathname.endsWith('.html')) {
+    const newPath = window.location.pathname.replace('.html', '');
+    window.history.replaceState(null, '', newPath);
+}
